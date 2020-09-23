d908bcdc6912ba60b66e139c23570e0600daf37f
mikaela_britoo <brito.mikaela36@gmail.com>
2020-09-23 12:44:55 -0300

    primeiro commit

---
+++ game.js
@@ -0,0 +1,28 @@
+const jogador = "X";
+const computador = "O";
+let gameOver = false;
+let playTime = "";
+
+function vezdojogador(){
+    playTime = document.getElementsByTagName("jogador");
+    playTime = document.querySelectorAll("div#header img")[0];
+    playTime.setAttribute("src", "circle.jpg");
+}
+function vezdoCPU(){
+    playTime = document.getElementsByTagName("computador");
+    playTime = document.querySelectorAll("div#header img")[0];
+    playTime.setAttribute("src", "x.jpg");
+}
+
+function quemJoga(){
+    if(document.getElementsByTagName("jogador" == "jogador")){
+        vezdojogador();
+    }else if (document.getElementsByTagName("computador") == "computador"){
+        vezdoCPU();
+    }
+}
+
+quemJoga();
+inicializarEspacos();
+jogar();
+cpu();
